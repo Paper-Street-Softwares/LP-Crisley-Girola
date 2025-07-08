98a5fb822a3bc624559dadf78f2996c2f9530429
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-08 08:04:51 -0300

    fix: Versão

--- src/components/sectionElements/hero/Quadrada.jsx
+++ src/components/sectionElements/hero/Quadrada.jsx
@@ -70,7 +70,7 @@ export default function Quadrada({ appDownloadButtons, colorMode }) {
                 {/* Observação */}
                 <MotionDivDownToUp>
                   <div className="flex justify-center desktop1:justify-start">
-                    <div className="flex flex-col items-center desktop1:flex-row text-white">
+                    <div className="flex flex-col items-center desktop1:flex-row text-black">
                       {content.texts.hero.obsHero.icon}
 
                       <p
